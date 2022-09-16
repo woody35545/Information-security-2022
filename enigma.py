74a19d2d5a697bea7c5689fe1e6f0f9f79c1eec9
Woody_K <84436996+woody35545@users.noreply.github.com>
2022-09-16 12:57:33 +0900

    Update enigma.py
    rotate_wheels() 수정 필요

--- enigma.py
+++ enigma.py
@@ -1,11 +1,16 @@
 from copy import deepcopy
 from ctypes import ArgumentError
 
+DEBUG_MODE = False
+def debug_msg(msg):
+    if DEBUG_MODE:
+        print("[DEBUG] " + msg)
+
 # Enigma Components
 ETW = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 WHEELS = {
-    "I" : {
+    "I": {
         "wire": "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
         "turn": 16
     },
@@ -34,6 +39,8 @@ SETTINGS = {
     "PLUGBOARD": []
 }
 
+
+
 def apply_settings(ukw, wheel, wheel_pos, plugboard):
     if not ukw in UKW:
         raise ArgumentError(f"UKW {ukw} does not exist!")
@@ -50,12 +57,13 @@ def apply_settings(ukw, wheel, wheel_pos, plugboard):
         if not wp in ETW:
             raise ArgumentError(f"WHEEL position must be in A-Z!")
         SETTINGS["WHEEL_POS"].append(ord(wp) - ord('A'))
-    
+
     plugboard_setup = plugboard.split(' ')
     for ps in plugboard_setup:
         if not len(ps) == 2 or not ps.isupper():
             raise ArgumentError(f"Each plugboard setting must be sized in 2 and caplitalized; {ps} is invalid")
         SETTINGS["PLUGBOARD"].append(ps)
+
 
 # Enigma Logics Start
 
@@ -69,33 +77,98 @@ def pass_plugboard(input):
 
     return input
 
+
 # ETW
 def pass_etw(input):
+    debug_msg(input)
+    debug_msg(f"[pass_etw] >> ord(input) - ord('A') = {ord(input) - ord('A')}")
+    debug_msg("[pass_etw] >> SETTINGS[\"ETW\"][ord(input) - ord('A')] = " + str(SETTINGS["ETW"][ord(input) - ord('A')]))
+
     return SETTINGS["ETW"][ord(input) - ord('A')]
 
+
 # Wheels
-def pass_wheels(input, reverse = False):
+def pass_wheels(input, reverse=False):
     # Implement Wheel Logics
     # Keep in mind that reflected signals pass wheels in reverse order
-    return input
+    encrypted = input
+    debug_msg("encrypted = " + str(input))
+    if reverse:
+        for wh in SETTINGS['WHEELS']:
+            #print(wh['wire'])
+            encrypted = wh['wire'][ord(wh['wire'][ord(encrypted) - ord('A')]) - ord('A')]
+            debug_msg("[pass_wheels] >> encrypted = wh['wire'][ord(wh['wire'][ord(encrypted) - ord('A')]) - ord('A')]: " + str(encrypted))
+    elif not reverse:
+        for i in range(0, len(SETTINGS['WHEELS'])):
+
+            encrypted = SETTINGS['WHEELS'][2-i]['wire'][ord(encrypted) - ord('A')]
+            debug_msg("[pass_wheels] >> encrypted = wh['wire'][ord(encrypted) - ord('A')] = " + str(encrypted))
+
+    return encrypted
+
 
 # UKW
 def pass_ukw(input):
+    debug_msg(f"[pass_ukw] >> ord(input) - ord('A') = {ord(input) - ord('A')}")
+    debug_msg("[pass_ukw] >> SETTINGS[\"UKW\"][ord(input) - ord('A')] = " + str(SETTINGS["UKW"][ord(input) - ord('A')]))
+
     return SETTINGS["UKW"][ord(input) - ord('A')]
+
 
 # Wheel Rotation
 def rotate_wheels():
     # Implement Wheel Rotation Logics
+    rotate_right_wheel()
+    if SETTINGS['WHEELS'][1]['wire'][0] == SETTINGS['WHEELS'][1]['turn']:
+        rotate_middle_wheel()
+    if SETTINGS['WHEELS'][0]['wire'][0] == SETTINGS['WHEELS'][0]['turn']:
+        rotate_left_wheel()
     pass
 
+def rotate_left_wheel():
+    global SETTINGS
+    debug_msg("LEFT WHEEL's wire= " + str(SETTINGS['WHEELS'][0]['wire']))
+    wire = str(SETTINGS['WHEELS'][0]['wire'])
+    rotated_wire = wire[1:len(wire)] + str(wire[0])
+    SETTINGS['WHEELS'][0]['wire'] = rotated_wire
+    #print(str(SETTINGS['WHEELS'][0]['wire']))
+    return None
+
+def rotate_middle_wheel():
+    middle_wheel = SETTINGS['WHEELS'][1]
+    wire = str(middle_wheel['wire'])
+    debug_msg("MIDDLE WHEEL's wire= " + str(wire))
+    rotated_wire = wire[1:len(wire)] + str(wire[0])
+    SETTINGS['WHEELS'][1]['wire'] = rotated_wire
+    #print(str(SETTINGS['WHEELS'][1]['wire']))
+    return None
+
+def rotate_right_wheel():
+    debug_msg("RIGHT WHEEL's wire= " + str(SETTINGS['WHEELS'][2]['wire']))
+    wire = str(SETTINGS['WHEELS'][2]['wire'])
+    rotated_wire = wire[1:len(wire)] + str(wire[0])
+    SETTINGS['WHEELS'][2]['wire'] = rotated_wire
+    #print(str(SETTINGS['WHEELS'][2]['wire']))
+    return None
+
+
 # Enigma Exec Start
+"""
 plaintext = input("Plaintext to Encode: ")
 ukw_select = input("Set Reflector (A, B, C): ")
 wheel_select = input("Set Wheel Sequence L->R (I, II, III): ")
 wheel_pos_select = input("Set Wheel Position L->R (A~Z): ")
 plugboard_setup = input("Plugboard Setup: ")
+"""
 
+
+plaintext = "AAA"
+ukw_select = "B"
+wheel_select = "III II I"
+wheel_pos_select = "A A A"
+plugboard_setup = "AA"
 apply_settings(ukw_select, wheel_select, wheel_pos_select, plugboard_setup)
+print(SETTINGS['WHEELS'])
 
 for ch in plaintext:
     rotate_wheels()
@@ -106,7 +179,9 @@ for ch in plaintext:
     encoded_ch = pass_etw(encoded_ch)
     encoded_ch = pass_wheels(encoded_ch)
     encoded_ch = pass_ukw(encoded_ch)
-    encoded_ch = pass_wheels(encoded_ch, reverse = True)
+    encoded_ch = pass_wheels(encoded_ch, reverse=True)
     encoded_ch = pass_plugboard(encoded_ch)
-
+    print("결과: " , end = " ")
     print(encoded_ch, end='')
+
+    print("")
