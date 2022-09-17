1d66d5cec2e8b5f46177eb701350f0ee9a878d89
Woody_K <84436996+woody35545@users.noreply.github.com>
2022-09-17 16:42:43 +0900

    Update enigma.py

--- enigma.py
+++ enigma.py
@@ -1,24 +1,10 @@
 from copy import deepcopy
 from ctypes import ArgumentError
 
-WHEELS = {
-    "I": {
-        "wire": "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
-        "state": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        "turn": 16
-    },
-    "II": {
-        "wire": "AJDKSIRUXBLHWTMCQGZNPYFVOE",
-        "state": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        "turn": 4
-    },
-    "III": {
-        "wire": "BDFHJLCPRTXVZNYEIWGAKMUSQO",
-        "state": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        "turn": 21
-    }
-}
-def get_mapping(wheel, reverse = False):
+
+def get_mapping(wheel, reverse=False):
+    # wire에 구조에 따라 Input이 어떤값으로 치환되는지에 대한 Mapping 값을 반환해줌
+    # 파라미터 설정에 따라 정방향 또는 역방향 Mapping 값을 반환.
     wire = wheel['wire']
     res = {}
     if reverse == False:
@@ -31,13 +17,14 @@ def get_mapping(wheel, reverse = False):
 
 
 def index_of(input_str, elemnt_to_find):
+    # 문자열에서 원하는 문자의 위치(index)를 찾아 반환해줌
     for i in range(len(input_str)):
         if input_str[i] == elemnt_to_find:
             return i
 
-# Enigma Components
-ETW =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+# Enigma Components
+ETW = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 WHEELS = {
     "I": {
@@ -71,7 +58,6 @@ SETTINGS = {
     "ETW": ETW,
     "PLUGBOARD": []
 }
-
 
 
 def apply_settings(ukw, wheel, wheel_pos, plugboard):
@@ -113,11 +99,7 @@ def pass_plugboard(input):
 # ETW
 def pass_etw(input, reverse=False):
     if reverse:
-        match_loc = index_of(SETTINGS['WHEELS'][2]['state'], input)
-        return SETTINGS["ETW"][match_loc]
-
-
-
+        return SETTINGS["ETW"][index_of(SETTINGS['WHEELS'][2]['state'], input)]
     return SETTINGS["ETW"][ord(input) - ord('A')]
 
 
@@ -134,21 +116,20 @@ def pass_wheels(input, reverse=False):
             if wheel == SETTINGS['WHEELS'][0]:
                 prev_state = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             else:
-                prev_state = SETTINGS['WHEELS'][i-1]['state']
+                prev_state = SETTINGS['WHEELS'][i - 1]['state']
 
         elif not reverse:
-            wheel = SETTINGS['WHEELS'][2-i]
+            wheel = SETTINGS['WHEELS'][2 - i]
             cur_state = wheel['state']
-            if wheel == SETTINGS['WHEELS'][(len(SETTINGS['WHEELS'])-1)]:
-                prev_state = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # SETTING['ETW']['state']
+            if wheel == SETTINGS['WHEELS'][(len(SETTINGS['WHEELS']) - 1)]:
+                prev_state = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # SETTING['ETW']['state']
             else:
-                prev_state = SETTINGS['WHEELS'][2-i+1]['state']
+                prev_state = SETTINGS['WHEELS'][2 - i + 1]['state']
         temp = encrypted
-        wire = wheel['wire']
 
         if reverse:
             match_char = cur_state[index_of(prev_state, encrypted)]
-            mapping = get_mapping(wheel,reverse = True)
+            mapping = get_mapping(wheel, reverse=True)
 
             encrypted = mapping[match_char]
 
@@ -157,38 +138,49 @@ def pass_wheels(input, reverse=False):
             match_char = cur_state[index_of(prev_state, encrypted)]
             encrypted = mapping[match_char]
 
-
     return encrypted
 
 
 # UKW
 def pass_ukw(input):
-
     return SETTINGS["UKW"][ord(input) - ord('A')]
+
+
+def rotate_wheel(opt='r'):
+    # 단일 Wheel에 대한 Rotate
+    global SETTINGS
+    if opt == 'r':
+        idx = 2
+    elif opt == 'm':
+        idx = 1
+    elif opt == 'l':
+        idx = 0
+    wheel = SETTINGS['WHEELS'][idx]
+    state = str(wheel['state'])
+    SETTINGS['WHEELS'][idx]['state'] = state[1:len(state)] + str(state[0])
+    return None
 
 
 # Wheel Rotation
 def rotate_wheels():
     # Implement Wheel Rotation Logics
-    rotate_wheel()
-    if SETTINGS['WHEELS'][2]['state'][-1] == chr(ord('A') + SETTINGS['WHEELS'][2]['turn']):
-        rotate_wheel(opt='m')
-    if SETTINGS['WHEELS'][1]['state'][-1] == chr(ord('A') + SETTINGS['WHEELS'][1]['turn']):
-        rotate_wheel(opt='l')
-    pass
+    # 각 Wheel이 Rotate 해야하는 지점에서 rotate_wheel을 호출하여 Rotate Logics를 구현함
 
-def rotate_wheel(opt = 'r'):
-    global SETTINGS
-    if opt == 'r':
-        wheel_idx = 2
-    elif opt == 'm':
-        wheel_idx = 1
-    elif opt == 'l':
-        wheel_idx = 0
-    wheel = SETTINGS['WHEELS'][wheel_idx]
-    state = str(wheel['state'])
-    SETTINGS['WHEELS'][wheel_idx]['state'] = state[1:len(state)] + str(state[0])
-    return None
+    rotate_wheel(opt='r')  # 가장 오른쪽 Wheel은 한글자를 Encrypt 하는 과정마다 조건없이 매번 돌아야함.
+
+    if SETTINGS['WHEELS'][2]['state'][-1] == chr(ord('A') + SETTINGS['WHEELS'][2]['turn']):
+        wheel = SETTINGS['WHEELS'][1]
+        state = str(wheel['state'])
+        SETTINGS['WHEELS'][1]['state'] = state[1:len(state)] + str(state[0])
+
+    if SETTINGS['WHEELS'][1]['state'][-1] == chr(ord('A') + SETTINGS['WHEELS'][1]['turn']):
+        wheel = SETTINGS['WHEELS'][0]
+        state = str(wheel['state'])
+        SETTINGS['WHEELS'][0]['state'] = state[1:len(state)] + str(state[0])
+
+
+pass
+
 
 # Enigma Exec Start
 """
@@ -199,19 +191,15 @@ wheel_pos_select = input("Set Wheel Position L->R (A~Z): ")
 plugboard_setup = input("Plugboard Setup: ")
 """
 
-
-plaintext = "A"*40
+plaintext = "ABCDEFGHIJKLMN"
 ukw_select = "B"
 wheel_select = "III II I"
 wheel_pos_select = "A A A"
-plugboard_setup = "AA"
+plugboard_setup = "DA"
 apply_settings(ukw_select, wheel_select, wheel_pos_select, plugboard_setup)
-
-
 
 for ch in plaintext:
     rotate_wheels()
-    #print("\nRIGHT WHEEL = " + SETTINGS["WHEELS"][2]['state'])
     encoded_ch = ch
     encoded_ch = pass_plugboard(encoded_ch)
     encoded_ch = pass_etw(encoded_ch)
@@ -221,5 +209,3 @@ for ch in plaintext:
     encoded_ch = pass_etw(encoded_ch, reverse=True)
     encoded_ch = pass_plugboard(encoded_ch)
     print(encoded_ch, end='')
-
-
