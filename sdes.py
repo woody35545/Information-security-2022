7de271ae4729eb981e749397a9ce155e3a44c7b4
Woody_K <84436996+woody35545@users.noreply.github.com>
2022-09-26 15:42:50 +0900

    sdes 구현중

--- sdes.py
+++ sdes.py
@@ -1,46 +1,47 @@
-# Simplified DES (Data Encryption Standard) 
-# S-DES Algorithm Template Code for CNU Information Security 2022
-
-# This code requires "bitarray" package.
-# Install with: pip install bitarray
-
 from ctypes import ArgumentError
 import re
 from bitarray import bitarray, util as ba_util
 
+DEBUG_MODE = True
+
+def dm(msg):
+    if DEBUG_MODE:
+        print("[DEBUG]", end=" ")
+        print(msg)
+
 # Initial Permutation (IP)
-IP = [ 1, 5, 2, 0, 3, 7, 4, 6 ]
+IP = [1, 5, 2, 0, 3, 7, 4, 6]
 
 # Inverse of Initial Permutation (or Final Permutation)
-IP_1 = [ 3, 0, 2, 4, 6, 1, 7, 5]
+IP_1 = [3, 0, 2, 4, 6, 1, 7, 5]
 
 # Expansion (4bits -> 8bits)
-EP = [ 3, 0, 1, 2, 1, 2, 3, 0 ]
+EP = [3, 0, 1, 2, 1, 2, 3, 0]
 
 # SBox (S0)
 S0 = [
-    [ 1, 0, 3, 2 ],
-    [ 3, 2, 1, 0 ],
-    [ 0, 2, 1, 3 ],
-    [ 3, 1, 3, 2 ]
+    [1, 0, 3, 2],
+    [3, 2, 1, 0],
+    [0, 2, 1, 3],
+    [3, 1, 3, 2]
 ]
 
 # SBox (S1)
 S1 = [
-    [ 0, 1, 2, 3 ],
-    [ 2, 0, 1, 3 ],
-    [ 3, 0, 1, 0 ],
-    [ 2, 1, 0, 3 ]
+    [0, 1, 2, 3],
+    [2, 0, 1, 3],
+    [3, 0, 1, 0],
+    [2, 1, 0, 3]
 ]
 
 # Permutation (P4)
-P4 = [ 1, 3, 2, 0 ]
+P4 = [1, 3, 2, 0]
 
 # Permutation (P10)
-P10 = [ 2, 4, 1, 6, 3, 9, 0, 8, 7, 5 ]
+P10 = [2, 4, 1, 6, 3, 9, 0, 8, 7, 5]
 
 # Permutation (P8)
-P8 = [ 5, 2, 6, 3, 7, 4, 9, 8 ]
+P8 = [5, 2, 6, 3, 7, 4, 9, 8]
 
 #### DES Start
 
@@ -52,7 +53,10 @@ schedule_keys: generate round keys for round function
 returns array of round keys.
 keep in mind that total rounds of S-DES is 2.
 '''
-def schedule_keys(key: bitarray) -> list[bitarray]:
+
+
+#def schedule_keys(key: bitarray) -> list[bitarray]:
+def schedule_keys(key: bitarray):
     round_keys = []
     permuted_key = bitarray()
 
@@ -78,55 +82,143 @@ def schedule_keys(key: bitarray) -> list[bitarray]:
 
         round_keys.append(round_key)
 
+
     return round_keys
+
 
 '''
 round: round function
 returns the output of round function
 '''
+
+
 def round(text: bitarray, round_key: bitarray) -> bitarray:
     # implement round function
+
     expanded = bitarray()
+
     for i in EP:
         expanded.append(text[i])
+
+    dm("expanded : " + str(expanded))
+    dm("round key: " + str(round_key))
     expanded ^= round_key
+
+    dm("expanded <- expanded ^ round key: " + str(expanded))
+
 
     # S0
     s0_row = expanded[0:4]
+    dm("expanded[0:4]: " + str(expanded[0:4]))
+
     s0_sel_row = (s0_row[0] << 1) + s0_row[3]
     s0_sel_col = (s0_row[1] << 1) + s0_row[2]
     s0_result = ba_util.int2ba(S0[s0_sel_row][s0_sel_col], length=2)
 
     # S1
     s1_row = expanded[4:8]
+    dm("expanded[4:8]: " + str(expanded[4:8]))
     s1_sel_row = (s1_row[0] << 1) + s1_row[3]
     s1_sel_col = (s1_row[1] << 1) + s1_row[2]
     s1_result = ba_util.int2ba(S1[s1_sel_row][s1_sel_col], length=2)
 
     pre_perm4 = s0_result + s1_result
-    
+    dm("pre_perm4: " + str(pre_perm4))
     result = bitarray()
     for i in P4:
         result.append(pre_perm4[i])
-
+    dm("result: " + str(result))
     return result
+
 
 '''
 sdes: encrypts/decrypts plaintext or ciphertext.
 mode determines that this function do encryption or decryption.
      MODE_ENCRYPT or MODE_DECRYPT available.
 '''
+
+
 def sdes(text: bitarray, key: bitarray, mode) -> bitarray:
-    result = bitarray()
+    # Create round keys using schedule_keys() function and assign them to "round_keys" variable
+    round_keys = schedule_keys(key)
+    dm("round_keys: " + str(round_keys)+"\n")
+    orders = [0]*len(round_keys)
+    if mode==MODE_ENCRYPT:
+        dm(">> ENCRYPT MODE <<")
+        for i in range(len(round_keys)):
+            orders[i] = i
+            
+    elif mode==MODE_DECRYPT:
+        dm(">> DECRYPT MODE <<")
+        for i in range(len(round_keys)):
+            orders[i] = len(round_keys)-1-i
+
+    dm("text: " + str(text))
+    dm("len(text): " + str(len(text)))
     
-    # Place your own implementation of S-DES Here
+    # According to the Initial Permutation(IP) table, it is executed once at the beginning
+    initial_permutated_text = bitarray()
+    for i in IP:
+        dm("ip[i] : " + str(i))
+        initial_permutated_text.append(text[i])
+
+    dm("initial permutated text: " + str(initial_permutated_text) + "\n")
+
+    # Split text into Left and Right
+    left_side = initial_permutated_text[0:4]
+    right_side = initial_permutated_text[4:8]
+
+    dm("splited_left_side: " + str(left_side))
+    dm("splited_right_side: " + str(right_side) + "\n")
+
+    """
+    [Round 1]
+    [Round 1] is performed using initail permutated text's left, right side and the first round key.
+    """
+    round_1_left_side = right_side
+    round_1_right_side = left_side ^ round(right_side, round_keys[orders[0]])
+
+    dm("round_1_left_side: " + str(round_1_left_side))
+    dm("round_1_right_side: " + str(round_1_right_side) + "\n")
     
+    """
+    [Round 2]
+    [Round 2] is performed using the result of round 1 and the second round key.
+    """
+    round_2_left_side = round_1_right_side
+    round_2_right_side = round_1_left_side ^ round(round_1_right_side, round_keys[orders[1]])
+    
+    dm("round_2_left_side: " + str(round_2_left_side))
+    dm("round_2_right_side: " + str(round_2_right_side) + "\n")
+
+    # Merge left and right into one bitarray
+    round_2_res = round_2_left_side + round_2_right_side
+    dm("round_2_res: " + str(round_2_res) + "\n")
+
+
+    # Inverse of initial permutation(IP_1) applied to the result of Round 2
+    inverse_initial_permutated_text = bitarray()
+
+    for i in IP_1:
+        #dm("i: " + str(i) + " round_2_res[" + str(i) + "]: " + str(round_2_res[i]))
+        inverse_initial_permutated_text.append(round_2_res[i])
+
+    dm("inverse_initial_permutated_text: " + str(inverse_initial_permutated_text) + "\n")
+
+    # Assign result to Result Bitarray
+    result = inverse_initial_permutated_text
+    dm("result: " + str(result) + "\n")
+
+
     return result
+
 
 #### DES Sample Program Start
 
-plaintext = input("[*] Input Plaintext in Binary (8bits): ")
-key = input("[*] Input Key in Binary (10bits): ")
+#plaintext = input("[*] Input Plaintext in Binary (8bits): ")
+#key = input("[*] Input Key in Binary (10bits): ")
+plaintext = "10001010"
+key = "1010001010"
 
 # Plaintext must be 8 bits and Key must be 10 bits.
 if len(plaintext) != 8 or len(key) != 10:
@@ -136,6 +228,7 @@ if re.search("[^01]", plaintext) or re.search("[^01]", key):
     raise ArgumentError("Inputs must be in binary!!!")
 
 bits_plaintext = bitarray(plaintext)
+print(f"bits_plaintext: {bits_plaintext}")
 bits_key = bitarray(key)
 
 print(f"Plaintext: {bits_plaintext}")
@@ -153,3 +246,4 @@ if result_decrypt != bits_plaintext:
     print(f"S-DES FAILED...")
 else:
     print(f"S-DES SUCCESS!!!")
+
