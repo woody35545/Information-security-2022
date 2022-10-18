f94a6dc530d66c792c800628b627b7f3bae870b6
Woody_K <84436996+woody35545@users.noreply.github.com>
2022-10-18 10:09:29 +0900

    Create rsa.py

---
+++ rsa.py
@@ -0,0 +1,60 @@
+# BigNumber, mpmath package required
+# run this before execute: pip install BigNumber mpmath
+
+import random
+from BigNumber import BigNumber
+
+
+# https://www.delftstack.com/howto/python/python-generate-prime-number/
+def primesInRange(x, y):
+    prime_list = []
+    for n in range(x, y):
+        isPrime = True
+
+        for num in range(2, n):
+            if n % num == 0:
+                isPrime = False
+
+        if isPrime:
+            prime_list.append(n)
+
+    return prime_list
+
+
+def make_keys(p: BigNumber, q: BigNumber):
+    # place your own implementation of make_keys
+    # use e = 65537 as if FIPS standard
+
+    return [e, d, n]
+
+
+def rsa_encrypt(plain: BigNumber, e: BigNumber, n: BigNumber):
+    # place your own implementation of rsa_encrypt
+    pass
+
+
+def rsa_decrypt(cipher: BigNumber, d: BigNumber, n: BigNumber):
+    # place your own implementation of rsa_decrypt
+    pass
+
+
+primes = primesInRange(100, 1000)
+
+P = primes[random.randrange(0, len(primes))]
+Q = primes[random.randrange(0, len(primes))]
+
+while P == Q:
+    P = primes[random.randrange(0, len(primes))]
+    Q = primes[random.randrange(0, len(primes))]
+
+M = random.randrange(2, 20)
+e, d, N = make_keys(P, Q)
+C = rsa_encrypt(M, e, N)
+M2 = rsa_decrypt(C, d, N)
+
+print(f"P = {P}, Q = {Q}, N = {N}, M = {M}, e = {e}, d = {d}, C = {C}, M2 = {M2}")
+
+if M == M2:
+    print("RSA Success!!")
+else:
+    print("RSA Failed...")
